8a51d330e8e6e8687d43b5af094b8a8ec6733d29
Rey Mark <reymark.egot.mgx@gmail.com>
2025-03-04 12:27:45 +0800

    task/part-1-task-eloquent-relation: initial commit

---
+++ app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    protected $fillable = ['user_id', 'title', 'description', 'price'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
