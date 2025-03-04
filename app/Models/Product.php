d976a54cd432d448665a55bd4f58bc606eb3b252
Rey Mark <reymark.egot.mgx@gmail.com>
2025-03-04 15:52:36 +0800

    task/task-4-advance-eloquent-queries: initial commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,11 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Scopes\ProductScope;
 class Product extends Model
 {
     //
     protected $fillable = ['user_id', 'title', 'description', 'price'];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new ProductScope());
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
